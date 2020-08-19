4c0ec5c4e75b19e1b0769601be239198b3f27566
Yogiraj <yogiraj.kadikar@gmail.com>
2020-08-20 00:45:25 +0530

    values cpp

---
+++ values.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string s1 = "C++", s2 = "Lang";
+    cout << s1+s2<<endl;
+    int x = 1+1;
+    float y = 7.0/3.0;
+    cout<< x<<endl;
+    cout<<y;
+    return 0;
+}
